d1ca0e9d22b8ac4bf8d17bca32997396d97eb9f2
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-07-02 23:59:00 +0900

    Commit `App` component

---
+++ src/App.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import React from 'react';
+import Header from './components/Header';
+import Profile from './components/Profile';
+import Portfolio from './components/Portfolio';
+import Skills from './components/Skills';
+import Music from './components/Music';
+import Footer from './components/Footer';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    shouldComponentUpdate() {
+        return true;
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Header />
+                <main>
+                    <Profile />
+                    <Portfolio />
+                    <Skills />
+                    <Music />
+                </main>
+            </React.Fragment>
+        );
+    }
+}
