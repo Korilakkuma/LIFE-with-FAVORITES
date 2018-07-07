5b9275e5c985e02555c9352419e4f39dd7cfe1a9
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-07-07 17:08:34 +0900

    Commit `Skills` component

---
+++ src/components/Skills/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import React from 'react';
+
+export default class Skills extends React.Component {
+    static CLASS_NAME = 'Skills';
+
+    constructor(props) {
+        super(props);
+    }
+
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    render() {
+        return (
+            <section className={Skills.CLASS_NAME}>
+                <div className="aligner">
+                    <h1>SKILLS</h1>
+                    <ul className="list-marker -white">
+                        <li>HTML, CSS</li>
+                        <li>JavaScript (ES2015 ~, TypeScript, Node.js)</li>
+                        <li>React, Angular</li>
+                        <li>PHP, Go</li>
+                        <li>C, C++, UNIX programming</li>
+                        <li>Shellscript</li>
+                        <li>MySQL, MongoDB</li>
+                        <li>Nginx, Docker</li>
+                        <li>macOS, Linux, Vim, zsh</li>
+                        <li>Sketch, Photoshop</li> 
+                    </ul>
+                </div>
+            </section>
+        );
+    }
+}
