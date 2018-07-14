c363a9cff2e4f761e5a038d4407f402d5843a9b5
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-07-15 03:46:25 +0900

    Fixed logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -18,7 +18,12 @@ export default class Header extends React.Component {
             <header className={Header.CLASS_NAME}>
                 <div className="aligner">
                     <h1><img src="assets/images/logo.png" alt="LIFE with FAVORITES" width="592" height="156" /></h1>
-                    {/* <h1>LIFE <span>with</span> FAVORITES</h1> /*}
+                    {/*
+                    <svg width="1182" height="312" title="LIFE with FAVORITES" role="img">
+                        <use xlinkHref="assets/images/logo.svg#life_favorite" />
+                    </svg>
+                    */}
+                    {/* <h1>LIFE <span>with</span> FAVORITES</h1> */}
                     {/* <h2>池田友洋 * Tomohiro IKEDA のオキニイリ生活サイト</h2> */}
                 </div>
             </header>
