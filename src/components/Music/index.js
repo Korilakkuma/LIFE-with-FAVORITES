1487765915c4225d237662ef786515620f88b259
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-07-14 21:45:00 +0900

    Fixed `Music` component

--- src/components/Music/index.js
+++ src/components/Music/index.js
@@ -7,42 +7,10 @@ export default class Music extends React.Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            index : 2
-        };
-
-        this.items = [
-            // <iframe key="cjLcjjfJmVw" width="560" height="315" src="https://www.youtube.com/embed/cjLcjjfJmVw" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>,
-            <iframe key="bcbDMOw2bGw" width="560" height="315" src="https://www.youtube.com/embed/bcbDMOw2bGw" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>,
-            <iframe key="7T8KGcuOu6I" width="560" height="315" src="https://www.youtube.com/embed/7T8KGcuOu6I" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>,
-            <iframe key="Xs_u5Cx5U1I" width="560" height="315" src="https://www.youtube.com/embed/Xs_u5Cx5U1I" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>,
-            <iframe key="ToWPdpeMT_w" width="560" height="315" src="https://www.youtube.com/embed/ToWPdpeMT_w" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>,
-            <iframe key="aCBxCotZA4A" width="560" height="315" src="https://www.youtube.com/embed/aCBxCotZA4A" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
-        ];
-
-        this.carousel = null;
-
-        this.onClickCarouselButton = this.onClickCarouselButton.bind(this);
     }
 
-    onClickCarouselButton(event) {
-        const button = event.currentTarget;
-        const index = parseInt(button.getAttribute('data-index'), 10);
-
-        const translateXMap = new Array(this.items.length);
-
-        for (let i = 0, len = translateXMap.length; i < len; i++) {
-            translateXMap[i] = 1120 - (i * 560);
-        }
-
-        this.carousel.style.transform = `translateX(${translateXMap[index]}px)`;
-
-        this.setState({ index });
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return this.state.index !== nextState.index;
+    shouldComponentUpdate() {
+        return false;
     }
 
     render() {
@@ -50,14 +18,14 @@ export default class Music extends React.Component {
             <section className={Music.CLASS_NAME}>
                 <div className="aligner">
                     <h1>MUSIC</h1>
-                    <div className={`${Music.CLASS_NAME}__carousel`}>
-                        <ol ref={node => this.carousel = node}>
-                            {this.items.map((item, index) => <li key={item.src} aria-hidden={index !== this.state.index}>{item}</li>)}
-                        </ol>
-                    </div>
-                    <ol className={`${Music.CLASS_NAME}__carouselSelector`}>
-                        {this.items.map((item, index) => <li key={item.src}><button data-index={index} type="button" onClick={this.onClickCarouselButton} disabled={index === this.state.index} aria-label={index + 1} className={index === this.state.index ? '-active' : ''}></button></li>)}
-                    </ol>
+                    <ul className={`${Music.CLASS_NAME}__grid`}>
+                        {/* <li><iframe key="cjLcjjfJmVw" width="560" height="315" src="https://www.youtube.com/embed/cjLcjjfJmVw" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li> */}
+                        <li><iframe key="Xs_u5Cx5U1I" width="560" height="315" src="https://www.youtube.com/embed/Xs_u5Cx5U1I" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li>
+                        <li><iframe key="bcbDMOw2bGw" width="280" height="157.5" src="https://www.youtube.com/embed/bcbDMOw2bGw" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li>
+                        <li><iframe key="ToWPdpeMT_w" width="280" height="157.5" src="https://www.youtube.com/embed/ToWPdpeMT_w" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li>
+                        <li><iframe key="7T8KGcuOu6I" width="280" height="157.5" src="https://www.youtube.com/embed/7T8KGcuOu6I" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li>
+                        <li><iframe key="aCBxCotZA4A" width="280" height="157.5" src="https://www.youtube.com/embed/aCBxCotZA4A" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li>
+                    </ul>
                 </div>
             </section>
         );
