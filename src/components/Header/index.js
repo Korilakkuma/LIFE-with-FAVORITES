ea86704c297711e4631fa0a71a98a07c8cbf2722
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-07-14 17:45:00 +0900

    Fixed `Header` component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,25 +17,9 @@ export default class Header extends React.Component {
         return (
             <header className={Header.CLASS_NAME}>
                 <div className="aligner">
-                    <h1>LIFE <span>with</span> FAVORITES</h1>
+                    <h1><img src="assets/images/logo.png" alt="LIFE with FAVORITES" width="592" height="156" /></h1>
+                    {/* <h1>LIFE <span>with</span> FAVORITES</h1> /*}
                     {/* <h2>池田友洋 * Tomohiro IKEDA のオキニイリ生活サイト</h2> */}
-                    <ul className={`${Header.CLASS_NAME}__sns`}>
-                        <li>
-                            <a href="https://github.com/Korilakkuma" target="_blank" rel="noopener noreferrer" className="image-link">
-                                <img src="assets/images/icon-github.png" alt="GitHub" width="64" height="64" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://twitter.com/xsoundjs" target="_blank" rel="noopener noreferrer" className="image-link">
-                                <img src="assets/images/icon-twitter.png" alt="Twitter" width="64" height="64" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://weblike-curtaincall.ssl-lolipop.jp/blog/" target="_blank" rel="noopener noreferrer" className="image-link">
-                                <img src="assets/images/icon-wordpress.png" alt="WordPress" width="64" height="64" />
-                            </a>
-                        </li>
-                    </ul>
                 </div>
             </header>
         );
