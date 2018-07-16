45184c98d7d336f6b9b2ecfd0a1e3bf764e8970b
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-07-16 16:47:25 +0900

    Add event handlers for starting animation again

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,30 +7,49 @@ export default class Header extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = { isAnimation : true };
+
+        this.onMouseEnter   = this.onMouseEnter.bind(this);
+        this.onAnimationEnd = this.onAnimationEnd.bind(this);
     }
 
-    shouldComponentUpdate() {
-        return false;
+    onMouseEnter() {
+        if (!this.state.isAnimation) {
+            this.setState({ isAnimation : true });
+        }
+    }
+
+    onAnimationEnd() {
+        if (this.state.isAnimation) {
+            this.setState({ isAnimation : false });
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.state.isAnimation !== nextState.isAnimation;
     }
 
     render() {
         return (
             <header className={Header.CLASS_NAME}>
                 <div className="aligner">
-                    <h1><img src="assets/images/logo.png" alt="LIFE with FAVORITES" width="592" height="156" /></h1>
+                    <h1 onMouseEnter={this.onMouseEnter}>
+                        <img src="assets/images/logo.png" alt="LIFE with FAVORITES" width="592" height="156" />
+                    </h1>
                     {/*
                     <svg width="1182" height="312" title="LIFE with FAVORITES" role="img">
                         <use xlinkHref="assets/images/logo.svg" />
                     </svg>
                     */}
                     {/* <h1>LIFE <span>with</span> FAVORITES</h1> */}
-                    <h2>
+                    <h2 onMouseEnter={this.onMouseEnter} className={this.state.isAnimation ? '-animation' : ''}>
                         Tomohiro IKEDA の
-                        <span role="presentation">オ</span>
-                        <span role="presentation">キ</span>
-                        <span role="presentation">ニ</span>
-                        <span role="presentation">イ</span>
-                        <span role="presentation">リ</span>
+                        <span onAnimationEnd={this.onAnimationEnd} role="presentation">オ</span>
+                        <span onAnimationEnd={this.onAnimationEnd} role="presentation">キ</span>
+                        <span onAnimationEnd={this.onAnimationEnd} role="presentation">ニ</span>
+                        <span onAnimationEnd={this.onAnimationEnd} role="presentation">イ</span>
+                        <span onAnimationEnd={this.onAnimationEnd} role="presentation">リ</span>
                         生活サイト
                     </h2>
                 </div>
