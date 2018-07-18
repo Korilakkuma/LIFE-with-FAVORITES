c6bad48362b37bc162ee63eee129ecc838f9f8e5
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-07-18 23:57:45 +0900

    Implement elastic video

--- src/components/Music/index.js
+++ src/components/Music/index.js
@@ -21,10 +21,10 @@ export default class Music extends React.Component {
                     <ul className={`${Music.CLASS_NAME}__grid`}>
                         {/* <li><iframe key="cjLcjjfJmVw" width="560" height="315" src="https://www.youtube.com/embed/cjLcjjfJmVw" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li> */}
                         <li><iframe key="Xs_u5Cx5U1I" width="560" height="315" src="https://www.youtube.com/embed/Xs_u5Cx5U1I" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li>
-                        <li><iframe key="bcbDMOw2bGw" width="280" height="157.5" src="https://www.youtube.com/embed/bcbDMOw2bGw" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li>
-                        <li><iframe key="ToWPdpeMT_w" width="280" height="157.5" src="https://www.youtube.com/embed/ToWPdpeMT_w" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li>
-                        <li><iframe key="7T8KGcuOu6I" width="280" height="157.5" src="https://www.youtube.com/embed/7T8KGcuOu6I" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li>
-                        <li><iframe key="aCBxCotZA4A" width="280" height="157.5" src="https://www.youtube.com/embed/aCBxCotZA4A" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li>
+                        {/* <li><iframe key="bcbDMOw2bGw" width="560" height="315" src="https://www.youtube.com/embed/bcbDMOw2bGw" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li> */}
+                        {/* <li><iframe key="ToWPdpeMT_w" width="560" height="315" src="https://www.youtube.com/embed/ToWPdpeMT_w" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li> */}
+                        {/* <li><iframe key="7T8KGcuOu6I" width="560" height="315" src="https://www.youtube.com/embed/7T8KGcuOu6I" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li> */}
+                        <li><iframe key="aCBxCotZA4A" width="560" height="315" src="https://www.youtube.com/embed/aCBxCotZA4A" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></li>
                     </ul>
                 </div>
             </section>
