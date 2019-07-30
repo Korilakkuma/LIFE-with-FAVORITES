d5f1094b3fbbc2ceaa2dc5da7f8699e40dddaa4e
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-07-30 22:00:00 +0900

    s/WEB/Web

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -20,7 +20,7 @@ export default class Profile extends React.Component {
                     <h1>PROFILE</h1>
                     <section>
                         <h1>NAME</h1>
-                        <p>WEB では, <b>Korilakkuma</b> や <b>rilakkuma3xjapan</b> として活動していることが多いです.</p>
+                        <p>Web では, <b>Korilakkuma</b> や <b>rilakkuma3xjapan</b> として活動していることが多いです.</p>
                     </section>
                     {/*
                     <section>
@@ -38,12 +38,12 @@ export default class Profile extends React.Component {
                             大学は肩書き上, 建築を専攻 (興味なしでしたが ...) .
                             大学 4 回生のときに, コンピュータサイエンスに興味をもち,
                             独学で勉強を開始. コンピュータサイエンスに専攻を変えて進学.
-                            研究は音情報処理で WEB とは無縁でしたが, 紆余曲折を経て,
-                            2012 年度に WEB プログラミング・WEB デザインに覚醒.
-                            WEB プログラミング・WEB デザインに関しては完全に独学です.
+                            研究は音情報処理で Web とは無縁でしたが, 紆余曲折を経て,
+                            2012 年度に Web プログラミング・Web デザインに覚醒.
+                            Web プログラミング・Web デザインに関しては完全に独学です.
                         </p>
                         <p>
-                            現在は, WEB フロントエンドエンジニアから転身して, マルチメディアエンジニアとなり, 動画・オーディオを技術を日々追求しています.
+                            現在は, Web フロントエンドエンジニアから転身して, マルチメディアエンジニアとなり, 動画・オーディオを技術を日々追求しています.
                         </p>
                     </section>
                     {/*
