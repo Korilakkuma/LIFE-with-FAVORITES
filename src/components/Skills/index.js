5294410fda731ef0f025fab232449a263e58024f
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-07-31 18:00:00 +0900

    Update skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -19,21 +19,21 @@ export default class Skills extends React.Component {
                 <div className="aligner">
                     <h1>SKILLS</h1>
                     <section>
+                        <h1>MULTIMEDIA</h1>
+                        <ul className="list-marker -white">
+                            <li>Web Music (Web Audio API,  WebRTC ... etc)</li>
+                            <li>hls.js / shaka-packager contributor</li>
+                            <li>C / C++, UNIX system call</li>
+                            <li>DSP (Digital Signal Processing)</li>
+                            <li>Computer Science</li>
+                        </ul>
+                    </section>
+                    <section>
                         <h1>WEB FRONTEND</h1>
                         <ul className="list-marker -white">
                             <li>HTML, CSS</li>
                             <li>JavaScript (ES2015 ~, TypeScript, Node.js)</li>
-                            <li>React, Redux</li>
                             <li>Sketch</li>
-                        </ul>
-                    </section>
-                    <section>
-                        <h1>MULTIMEDIA</h1>
-                        <ul className="list-marker -white">
-                            <li>Web Audio API, Web MIDI API, WebRTC ... etc</li>
-                            <li>hls.js</li>
-                            <li>C, C++, UNIX system call</li>
-                            <li>DSP (Digital Signal Processing)</li>
                         </ul>
                     </section>
                 </div>
