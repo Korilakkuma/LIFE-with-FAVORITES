9e692a4af37919ec2435f54a399208be0ff69588
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-08-03 20:00:00 +0900

    Update skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -21,18 +21,16 @@ export default class Skills extends React.Component {
                     <section>
                         <h1>MULTIMEDIA</h1>
                         <ul className="list-marker -white">
-                            <li>Web Music (Web Audio API, WebRTC ...)</li>
                             <li>Audio Signal Processing</li>
-                            <li>Streaming (<a href="https://github.com/video-dev/hls.js/graphs/contributors" target="_blank" rel="noopener noreferrer">&#064;hls.js</a> / <a href="https://github.com/google/shaka-packager/graphs/contributors" target="_blank" rel="noopener noreferrer">&#064;shaka-packager</a>)</li>
-                            <li>Computer Science</li>
                             <li>C / C++, UNIX system call</li>
+                            <li>Web Music (Web Audio API, WebRTC ...)</li>
                         </ul>
                     </section>
                     <section>
                         <h1>WEB FRONTEND</h1>
                         <ul className="list-marker -white">
                             <li>HTML / CSS</li>
-                            <li>JavaScript (ES2015 ~ / TypeScript / Node.js)</li>
+                            <li>JavaScript (TypeScript / Node.js)</li>
                             <li>Sketch</li>
                         </ul>
                     </section>
