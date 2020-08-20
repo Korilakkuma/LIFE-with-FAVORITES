79eb6ea688d024ea396163d925cc3cb8996a98a4
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-08-20 17:27:12 +0900

    Remove `role="alert"`
    Because this should not be used with `aria-live="assertive"`.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -204,7 +204,7 @@ export class Footer extends React.Component {
             </form>
             {errorMessages.length > 0
               ? <ul className={`${Footer.CLASS_NAME}__errors list-marker -white`}>
-                {errorMessages.map(message => <li key={message} role="alert" aria-live="assertive">{message}</li>)}
+                {errorMessages.map(message => <li key={message} aria-live="assertive">{message}</li>)}
               </ul> : null}
             {successMessages.length > 0
               ? <ul className={`${Footer.CLASS_NAME}__success list-marker -white`}>
